e7cdab387830b7c4ad51e6e2a1a5c7e156dc8ae3
sanidhya-w09 <sanidhyawanjari78@gmail.com>
2025-04-12 18:43:17 +0530

    Second Commit

--- server.js
+++ server.js
@@ -65,4 +65,5 @@ initializeDatabase()
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+  console.log('http://localhost:3000');
 });
